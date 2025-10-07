565fb715d9e6380829f6eaf0d10941067e82008f
JHinGalv <jhingal3010@ieszaidinvergeles.org>
2025-10-07 19:37:22 +0200

    UPDATE provision-dhcpsv.sh     - Added the config to /etc/dhcp/dhcpd.conf     - Added checks to the service state

--- provision-dhcpsv.sh
+++ provision-dhcpsv.sh
@@ -21,3 +21,28 @@ systemctl restart isc-dhcp-server
 #Hacemos backup del archivo de configuración DHCP
 cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak
 
+#Añadimos la configuración del DHCP al archivo
+cat > /etc/dhcp/dhcpd.conf <<EOF
+default-lease-time 86400;   # 1 día (en segundos)
+max-lease-time 691200;      # 8 días (en segundos)
+authoritative;              # Pues es el único sv
+
+subnet 192.168.57.0 netmask 255.255.255.0 {
+  range 192.168.57.25 192.168.57.50;
+  option routers 192.168.57.10;
+  option subnet-mask 255.255.255.0;
+  option broadcast-address 192.168.57.255;
+  option domain-name-servers 8.8.8.8, 4.4.4.4;
+  option domain-name "micasa.es";
+}
+EOF
+
+# Reiniciamos y habilitamos el servicio para aplicar la configuración
+systemctl restart isc-dhcp-server.service
+systemctl enable isc-dhcp-server.service
+
+# Comprobamos que se ha iniciado correctamente
+systemctl status isc-dhcp-server.service --no-pager # --no-pager evita interrupciones
+
+# Comprobamos que el socket está escuchando
+ss -lun
