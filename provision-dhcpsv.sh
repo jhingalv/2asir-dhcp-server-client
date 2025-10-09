4e443569a7abb8c6c1cbe152085c5c2e7cf9e82f
JHinGalv <jhingal3010@ieszaidinvergeles.org>
2025-10-10 00:38:27 +0200

    UPDATE provison-dhcpsv.sh     - Added MAC based DHCP provision for c2

--- provision-dhcpsv.sh
+++ provision-dhcpsv.sh
@@ -21,12 +21,14 @@ systemctl restart isc-dhcp-server
 # Make a backup of the DHCP config file
 cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak
 
-# Add the DHCP config to the file
+# Add the DHCP config
+
 cat > /etc/dhcp/dhcpd.conf <<EOF
 default-lease-time 86400;   # 1 day
 max-lease-time 691200;      # 8 days
 authoritative;              # As it is the only DHCP server
 
+# General DHCP config
 subnet 192.168.57.0 netmask 255.255.255.0 {
   range 192.168.57.25 192.168.57.50;
   option routers 192.168.57.10;
@@ -34,6 +36,14 @@ subnet 192.168.57.0 netmask 255.255.255.0 {
   option broadcast-address 192.168.57.255;
   option domain-name-servers 8.8.8.8, 4.4.4.4;
   option domain-name "micasa.es";
+}
+
+# Fixed IP for VM c2
+host c2 {
+  hardware ethernet 08:00:27:c2:c2:c2;
+  fixed-address 192.168.57.4;
+  default-lease-time 3600;  # 1 hour
+  option domain-name-servers 1.1.1.1;
 }
 EOF
 
